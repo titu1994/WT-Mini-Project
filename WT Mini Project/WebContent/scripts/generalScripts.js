64f011bc912dd333c23038f5f0b771f5058e59da
ishaanj <ishjain321@gmail.com>
2014-10-08 15:56:24 +0530

    generalScripts file modified into jquery

--- WT Mini Project/WebContent/scripts/generalScripts.js
+++ WT Mini Project/WebContent/scripts/generalScripts.js
@@ -1,33 +1,19 @@
 var sw = 1;
-
-function openButton() {
-	if(sw == 1) {
-		sw = 0;
-		document.getElementsByClassName('link_button')[0].style.opacity='0.3';
-		document.getElementsByClassName('translucent_box')[0].style.display = 'none';
-		document.getElementsByClassName('index')[0].style.display = 'none';
-		document.getElementsByClassName('lines')[0].style.display = 'none';
-
-		expandContent(document.getElementsByClassName('centerBox')[0]);	
-	}
-	else {
-		sw = 1;
-		document.getElementsByClassName('link_button')[0].style.opacity='1.0';
-		document.getElementsByClassName('translucent_box')[0].style.display = 'block';
-		document.getElementsByClassName('index')[0].style.display = 'block';
-		document.getElementsByClassName('lines')[0].style.display = 'block';
-		
-		expandContent(document.getElementsByClassName('centerBox')[0]);	
-	}
-}
-
-function expandContent(element) {
-	if(sw == 0) {
-		element.style.width = '100%';
-		element.style.left = '0%';
-	}
-	else {
-		element.style.width = '78%';
-		element.style.left = '20%';
-	}
-}
+$(document).ready(function(){
+	$(".link_button").click(function(){
+		if(sw == 1) {
+			sw = 0;
+			$(".link_button").css("opacity","0.3");
+			$(".index").fadeOut("fast");
+			$(".translucent_box").animate({width: 'toggle'});
+			$(".lines_vert").animate({left:'1%'});
+		}
+		else {
+			sw = 1;
+			$(".link_button").css("opacity","1");
+			$(".index").fadeIn();
+			$(".translucent_box").animate({width: 'toggle'});
+			$(".lines_vert").animate({left:'21.5%'});
+		}
+	});
+});	
