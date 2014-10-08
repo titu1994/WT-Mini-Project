0cbf5ecf6dfc78774bcf573791fea14bce7f0f43
Somshubra Majumdar <titu1994@gmail.com>
2014-10-08 18:00:25 +0530

    Enabled Start Scripts

---
+++ WT Mini Project/WebContent/scripts/startScripts.js
@@ -0,0 +1,24 @@
+/*var sw = 1;
+$(document).ready(function(){
+	$(".link_button").click(function(){
+		if(sw == 1) {
+			sw = 0;
+			
+			$(".header1").animate({left:'10%'});
+			$(".trailer1").animate({left:'10%'});
+			$(".trailer2").animate({left:'10%'});
+			$(".trailer3").animate({left:'10%'});
+			$(".trailer4").animate({left:'10%'});
+		}
+		else {
+			sw = 1;
+		
+			$(".header1").animate({left:'21.5%'});
+			$(".trailer1").animate({left:'21.5%'});
+			$(".trailer2").animate({left:'21.5%'});
+			$(".trailer3").animate({left:'21.5%'});
+			$(".trailer4").animate({left:'21.5%'});
+		}
+	});
+
+});	*/
